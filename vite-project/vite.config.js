b88a0a3fb39252770d4cdb853f4fb35a185bdfdd
AsmiiGaurav <agaurav_be22@thapar.edu>
2025-11-21 00:59:01 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/Portfolio-website-/", // 👈 MUST match your repo name exactly
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
