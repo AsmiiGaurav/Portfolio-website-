ac73873ed8eb4c227b7ddd5b7b283215189394c1
AsmiiGaurav <agaurav_be22@thapar.edu>
2025-11-21 00:39:59 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,15 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio-website-/", // 👈 IMPORTANT for GitHub Pages
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-})
+  base: "/Portfolio-website-/", // 👈 MUST match your repo name exactly
+});
